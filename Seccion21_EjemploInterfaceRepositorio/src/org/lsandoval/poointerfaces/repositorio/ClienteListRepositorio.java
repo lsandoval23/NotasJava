0a2d8916103d135e0b6db15788f102e5349efc66
Luis Sandoval <lsandovalancco@gmail.com>
2022-07-10 19:56:21 -0500

    Se agrega la implementación de las interfaces Paginable y Ordenable. Se hace el uso del metodo sort de ArrayList que usa la interfaz Comparator. (Para mas detalles averguar dentro del API de coleccion de JAVA)

--- Seccion21_EjemploInterfaceRepositorio/src/org/lsandoval/poointerfaces/repositorio/ClienteListRepositorio.java
+++ Seccion21_EjemploInterfaceRepositorio/src/org/lsandoval/poointerfaces/repositorio/ClienteListRepositorio.java
@@ -3,6 +3,7 @@ package org.lsandoval.poointerfaces.repositorio;
 import org.lsandoval.poointerfaces.modelo.Cliente;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 // Equivalente al DAO implementation que se usa en Colossus, por defecto la libreria CRUD(Framework de persistencia)
@@ -61,12 +62,36 @@ public class ClienteListRepositorio implements CrudRepositorio, PaginableReposit
     // Paginable
     @Override
     public List<Cliente> listar(int desde, int hasta) {
-        return null;
+        return datasource.subList(desde, hasta);
     }
 
     //Ordenable
     @Override
     public List<Cliente> listar(String campo, Direccion dir) {
-        return null;
+        datasource.sort((a,b) -> {
+            int resultado = 0;
+            if (dir == Direccion.ASC){
+                switch (campo){
+                    case "id":
+                        resultado = a.getId().compareTo(b.getId());
+                    case "nombre":
+                        resultado = a.getNombre().compareTo(b.getNombre());
+                    case "apellido":
+                        resultado = a.getApellido().compareTo(b.getApellido());
+                }
+            } else if (dir == Direccion.DESC){
+                switch (campo){
+                    case "id":
+                        resultado = b.getId().compareTo(a.getId());
+                    case "nombre":
+                        resultado = b.getNombre().compareTo(a.getNombre());
+                    case "apellido":
+                        resultado = b.getApellido().compareTo(a.getApellido());
+                }
+            }
+            return resultado;
+        });
+
+        return datasource;
     }
 }
