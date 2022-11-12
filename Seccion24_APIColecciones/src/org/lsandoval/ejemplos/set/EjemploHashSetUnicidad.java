8a35c8dad1d0db275955f957aa076a0deef7304d
Luis Sandoval <lsandovalancco@gmail.com>
2022-11-12 15:39:10 -0500

    Agregando ejemplos de iteraciones sobre Set y List.

--- Seccion24_APIColecciones/src/org/lsandoval/ejemplos/set/EjemploHashSetUnicidad.java
+++ Seccion24_APIColecciones/src/org/lsandoval/ejemplos/set/EjemploHashSetUnicidad.java
@@ -2,8 +2,7 @@ package org.lsandoval.ejemplos.set;
 
 import org.lsandoval.ejemplos.modelo.Alumno;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class EjemploHashSetUnicidad {
     public static void main(String[] args) {
@@ -11,7 +10,7 @@ public class EjemploHashSetUnicidad {
         // Por defecto si no implementamos los metodos equals and hashcode en la clase Alumno
         // Comparara las direcciones de memoria para ver si los elementos son iguales
 
-        Set<Alumno> sa = new HashSet<>();
+        List<Alumno> sa = new ArrayList<>();
 
         sa.add(new Alumno("Pato", 5));
         sa.add(new Alumno("Cata", 6));
@@ -25,6 +24,35 @@ public class EjemploHashSetUnicidad {
         sa.add(new Alumno("Zeus", 2));
 
         System.out.println("sa = " + sa);
+
+
+        /** Iterando sobre colecciones **/
+
+        // El metodo get solo aplica para las listas, los set al no tener un orden no tienen un indice
+        // Con sets no se pueden hacer iteraciones con un for clasico
+        System.out.println("Utilizando un for clasico");
+        for (int i=0; i<sa.size(); i++){
+            Alumno a = sa.get(i);
+            System.out.println(a.getNombre());
+        }
+
+
+        // Estos metodos aplican tanto para list (ArrayList) como para set (HashSet, TreeSet)
+        System.out.println("Iterando usando foreach");
+        for (Alumno a: sa){
+            System.out.println(a.getNombre());
+        }
+
+        System.out.println("Iterando usando while y iterator");
+        Iterator<Alumno> it = sa.iterator();
+        while (it.hasNext()){
+            Alumno a = it.next();
+            System.out.println(a.getNombre());
+        }
+
+        System.out.println("Iterando usando streams");
+        sa.forEach(System.out::println);
+
 
     }
 }
