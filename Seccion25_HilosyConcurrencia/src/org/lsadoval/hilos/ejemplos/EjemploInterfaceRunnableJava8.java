363c157a373833ff4f70fafd2ccb8557d438987b
Luis Sandoval <lsandovalancco@gmail.com>
2022-12-19 11:51:14 -0500

    Ejemplo de uso del metodo join.

--- Seccion25_HilosyConcurrencia/src/org/lsadoval/hilos/ejemplos/EjemploInterfaceRunnableJava8.java
+++ Seccion25_HilosyConcurrencia/src/org/lsadoval/hilos/ejemplos/EjemploInterfaceRunnableJava8.java
@@ -2,7 +2,7 @@ package org.lsadoval.hilos.ejemplos;
 
 public class EjemploInterfaceRunnableJava8 {
     // Como la interfaz Runnable es funcional se puede hacer su implementacion usando expresiones lambda
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         // El metodo estatico currentThread devuelve una instancia del hilo actual, en este caso el hilo main
         Thread main = Thread.currentThread();
@@ -11,7 +11,7 @@ public class EjemploInterfaceRunnableJava8 {
         Runnable viaje = () -> {
 
             for(int i=0; i < 10; i++){
-                // En este caso el metodo currentThread hara referencia al hilo sobre el que se instacie
+                // En este caso el metodo currentThread hara referencia al hilo sobre el que se instacie, no el metodo main
                 System.out.println(i + " - " + Thread.currentThread().getName());
                 try {
                     Thread.sleep( (long)(Math.random() * 1000));
@@ -28,6 +28,14 @@ public class EjemploInterfaceRunnableJava8 {
         Thread v2 = new Thread(viaje, "Robinson Crusoe");
         v2.start();
 
+        // El metodo join asocia el hilo v1 con el hilo actual (main en este caso) para que se detenga hasta que el hilo
+        // v1 termine su ejecucion
+
+
+        v1.join();
+        v2.join();
+
+        // Una vez que termine la ejecucion de v1 y v2, se procede con el hilo main
         System.out.println("Continuando con la ejecución del método main: " + main.getName());
 
 
