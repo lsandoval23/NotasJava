8cd68b0c22becb8a797856a104150ef811b4d4b5
Luis Sandoval <lsandovalancco@gmail.com>
2023-05-23 20:53:52 -0500

    - Agregamos una clase repositorio para producto, para tener un CRUD para esta entidad. Usamos argumentos genericos para extender la solucion a otro tipo de modelo - En la clase main, nos aseguramos de cerrar la conexion a la base de datos.

--- Seccion34_jdbc/src/main/java/org/lsandoval/EjemploJdbc.java
+++ Seccion34_jdbc/src/main/java/org/lsandoval/EjemploJdbc.java
@@ -1,5 +1,8 @@
 package org.lsandoval;
 
+import org.lsandoval.modelo.Producto;
+import org.lsandoval.repositorio.ProductoRepositorioImpl;
+import org.lsandoval.repositorio.Repositorio;
 import org.lsandoval.util.ConexionBaseDatos;
 
 import java.sql.*;
@@ -7,23 +10,13 @@ import java.sql.*;
 public class EjemploJdbc {
     public static void main(String[] args) {
 
-        // Usamos un bloque try con uso de recursos para asegurarnos de que las conexiones a la base de datos
-        // se cierren así ocurran excepciones en el transcurso de la ejecucion
+        // Nos aseguramos de cerrar la conexion de base de datos en este bloque de programa, en los repositorios
+        // no se están cerrando.
 
-        try (Connection conn = ConexionBaseDatos.getInstance();
-             Statement stmt = conn.createStatement();
-             ResultSet result = stmt.executeQuery("SELECT * FROM productos")) {
+        try (Connection conn = ConexionBaseDatos.getInstance()){
 
-            // Dentro del objeto result se obtiene un puntero a cada registro que se obtuvo de la consulta.
-            while (result.next()){
-                System.out.print(result.getInt("id"));
-                System.out.print("|");
-                System.out.print(result.getString("nombre"));
-                System.out.print("|");
-                System.out.print(result.getInt("precio"));
-                System.out.print("|");
-                System.out.println(result.getDate("fecha_registro"));
-            }
+            Repositorio<Producto> repositorio = new ProductoRepositorioImpl();
+            repositorio.listar().forEach(p -> System.out.println(p.getNombre()));
 
         } catch (SQLException e) {
             e.printStackTrace();
---
+++ Seccion34_jdbc/src/main/java/org/lsandoval/repositorio/ProductoRepositorioImpl.java
@@ -0,0 +1,56 @@
+package org.lsandoval.repositorio;
+
+import org.lsandoval.modelo.Producto;
+import org.lsandoval.util.ConexionBaseDatos;
+
+import javax.swing.plaf.nimbus.State;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductoRepositorioImpl implements Repositorio<Producto>{
+
+    private Connection getConnection() throws SQLException {
+        return ConexionBaseDatos.getInstance();
+    }
+
+    @Override
+    public List<Producto> listar() {
+        List<Producto> productos = new ArrayList<>();
+        try (Statement stmt = getConnection().createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM productos")){
+
+            while (rs.next()){
+                Producto p = new Producto();
+                p.setId(rs.getLong("id"));
+                p.setNombre(rs.getString("nombre"));
+                p.setPrecio(rs.getInt("precio"));
+                p.setFechaRegistro(rs.getDate("fecha_registro"));
+
+                productos.add(p);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return productos;
+    }
+
+    @Override
+    public Producto porId(Long id) {
+        return null;
+    }
+
+    @Override
+    public void guardar(Producto producto) {
+
+    }
+
+    @Override
+    public void eliminar(Long id) {
+
+    }
+}
